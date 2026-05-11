c088ed34a94f0f310ad12931dd346e71a3e9885a
Debangshi-riri <debangshiroychowdhury1912@gmail.com>
2026-05-11 19:04:20 +0000

    git commit -m space

--- src/com/quiz/DBConn.java
+++ src/com/quiz/DBConn.java
@@ -9,7 +9,7 @@ public class DBConn
         try
         {
             Class.forName("com.mysql.cj.jdbc.Driver");
-            con = DriverManager.getConnection("jdbc:mysql://localhost:3306/college_quiz", "root", "");
+            con = DriverManager.getConnection("jdbc:mysql://localhost:3306/college_quiz", "root", " ");
         } 
         catch (Exception e)
         {
